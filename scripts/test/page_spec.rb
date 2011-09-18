43e22c6f53451b65f51dac86fd625fa2c43a3f61
Fred McHale <binaryfever@gmail.com>
2011-09-17 21:29:41 -0700

    Added rspect(watir) tests

---
+++ scripts/test/page_spec.rb
@@ -0,0 +1,29 @@
+require "rubygems"
+require "rspec"
+require "watir-webdriver"
+
+describe "binaryfever.com" do
+  let(:browser){ @browser ||= Watir::Browser.new :firefox }
+
+  before{ browser.goto "file:///Users/fred/Development/binaryfever.com/index.html"}
+  after{ browser.close }
+
+  it "should be a valid URL" do
+    browser.text.should_not include ('The requested URL could not be retrieved')
+  end
+
+  it "should open about section when about link is clicked" do
+	browser.link(:id => "about_link").click
+    browser.section(:id => "about").text.should include("About")
+  end
+
+  it "should open projects section when projects link is clicked" do
+    browser.link(:id => "projects_link").click
+    browser.section(:id => "projects").text.should include("Projects")
+  end
+
+  it "should open contact section when contact link is clicked" do
+    browser.link(:id => "contact_link").click
+    browser.section(:id=> "contact").text.should include("Contact")
+  end
+end
